fecc2edeb16c0d3f9dab3b11aeb5beca22b2c67b
Tyler Schoen <tyler@ethode.com>
2016-11-23 15:05:18 -0500

    Added basic feed retrieval and layout of page. Login modal exists but isn't functional, same goes for filtering

---
+++ server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.register(require('inert'), (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+            reply.file('./index.html');
+        }
+    });
+});
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
